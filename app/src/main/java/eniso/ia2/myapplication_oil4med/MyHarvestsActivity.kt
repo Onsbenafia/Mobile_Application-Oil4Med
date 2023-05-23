b94c08de28ef08f243c51e7a066dd9ca1d34e67f
Onsbenafia <108668887+Onsbenafia@users.noreply.github.com>
2023-05-23 19:51:08 +0100

    Second commit

---
+++ app/src/main/java/eniso/ia2/myapplication_oil4med/MyHarvestsActivity.kt
@@ -0,0 +1,11 @@
+package eniso.ia2.myapplication_oil4med
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyHarvestsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_harvests)
+    }
+}
